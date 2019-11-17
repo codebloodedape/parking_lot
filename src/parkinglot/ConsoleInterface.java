d30ed8c6e3b31ce730af52cd98d4c3d2c0303a05
Vishal Chandran <vishhin@gmail.com>
2019-11-17 20:15:26 +0530

    ConsoleInterface class - resposible for reading commands from file/standard input and writing to standard output

---
+++ src/parkinglot/ConsoleInterface.java
@@ -0,0 +1,63 @@
+package parkinglot;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ConsoleInterface {
+
+	/**
+	 * Main method of the entry Class ConsoleInterface. 
+	 * This method will take the first argument, if provided, for the path of the command file,
+	 * else, read commands from the console
+	 * @param args array of arguments. The command file path should be the first argument in the array.
+	 */
+	public static void main(String []args) {
+        if (args.length == 0)
+        {
+            System.out.println("Interactive mode");
+            Scanner stdInScanner = new Scanner(System.in);
+            try {
+            	System.out.println("Enter commands/queries below(enter 'exit' to quit):");
+            	String line;
+            	
+        		while (true) {
+        			line = stdInScanner.nextLine().trim();
+        			if (line.equalsIgnoreCase("EXIT")) {
+        				System.out.println("Exiting.");
+        				return;
+        			}
+        			processQuery(line);
+        		}
+            }
+            finally {
+            	stdInScanner.close();
+            }
+        }
+        else
+        {
+            String commandFilePath = args[0];
+            System.out.println("File mode");
+			try {
+				Scanner fileScanner = new Scanner(new File(commandFilePath));
+				System.out.println("Commands/Queries are read from " + commandFilePath + ".");
+				while (fileScanner.hasNextLine()) {
+					String line = fileScanner.nextLine();
+					System.out.println(line);
+					processQuery(line);
+				}
+				fileScanner.close();
+			} catch (FileNotFoundException e) {
+				System.out.println("The file " + commandFilePath + " could not be founud. "
+						+ "Check if the command file exists in the specified location before "
+						+ "restarting this application.");
+				e.printStackTrace();
+			}
+        }
+	}
+	
+	private static void processQuery(String line)
+    {
+		System.out.println("Executing query " + line);
+    }
+}
